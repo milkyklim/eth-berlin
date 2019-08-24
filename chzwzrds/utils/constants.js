458331737311148e3399d51dd8bdee405ecc0f02
milkyklim <klim.kolyvanov@gmail.com>
2019-08-24 06:39:51 +0200

    Add simple test: summon one neutral wizard

--- chzwzrds/utils/constants.js
+++ chzwzrds/utils/constants.js
@@ -1,8 +1,10 @@
 const bn = require('bn.js');
 
 module.exports = {
-  STARTING_COST: new bn('70000000000000000'), // +
-  COST_INCREMEMENT: 130, // +
+  STARTING_COST: new bn('70000000000000000'),
+  COST_INCREMEMENT: 130,
   EXCLUSIVE_COUNT: 1234,
   SALE_DURATION: 47615,
+  NEUTRAL_WIZARD_COST: new bn('70000000000000000'),
+  POWER_SCALE: new bn('1000'),
 };
